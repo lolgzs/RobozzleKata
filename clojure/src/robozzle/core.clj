ab5757e389c0b320036803bbfcd1bde3fdef307f
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2012-03-28 09:32:26 +0200

    Kata in Clojure: supports now several identique movements (forward)

--- clojure/src/robozzle/core.clj
+++ clojure/src/robozzle/core.clj
@@ -1,26 +1,39 @@
 (ns robozzle.core)
 
+; A position is defined by its coordinates in the space
 (defrecord Position [x y])
 
-;; position in the space
+; A space ship is defined by its path in the space
+(defrecord SpaceShip [movement])
+
+;; Position in the space
 (defn at [x y]
   (Position. x y))
 
-; moves the ship according to the specified command and returns the new
-; position of the ship. If no command is given, then the current position
-; of the ship is returned.
-(defn move-ship [at-position command]
-  (cond 
-    (= nil command) at-position
-    (= :forward command) (Position. 1 0)
-    :else (throw (java.lang.Exception. "Unknown command"))))
+; Moves the specified ship to the specified position and returns the ship with
+; its updated position.
+(defn move-ship [ship to-position]
+  (SpaceShip. (conj (:movement ship) to-position)))
 
-; is the level completed by the ship once it has performed the specified
+; Applies the specified command to the given space ship and returns the ship
+; updated with its new position.
+; If no command is given, then the current position
+; of the ship is returned.
+(defn command-ship [ship command]
+  (let [position (peek (:movement ship))]
+    (cond 
+      (= nil command)      ship
+      (= :forward command) (move-ship ship (at (+ (:x position) 1) (:y position)))
+      :else (throw (java.lang.Exception. "Unknown command")))))
+
+; Computes the travel path of a ship in the space starting from the specified
+; initial position and by applying to it the specified ship commands
+(defn ship-trail [initial-position commands]
+  (:movement (reduce command-ship (SpaceShip. [initial-position]) commands)))
+
+; Is the level completed by the ship once it has performed the specified
 ; commands?
 ; The level is completed only if it catches the stars at the specified
-; positions and within the indicated maximum movements.
-(defn level-completed? [max-attempts positions commands]
-  (cond
-    (and (empty? positions) (empty? commands)) true
-    :else (= (move-ship (at 0 0) (first commands)) (first positions))))
-
+; positions and within the given number of maximum movements.
+(defn level-completed? [max-movements positions commands]
+    (empty? (clojure.set/difference (into #{} positions) (into #{} (ship-trail (at 0 0) commands)))))
--- clojure/test/robozzle/test/core.clj
+++ clojure/test/robozzle/test/core.clj
@@ -11,7 +11,8 @@
 (deftest no-move-with-one-start-off-the-initial-position-doesnt-complete-the-level
     (is (not (level-completed? 0 [(at 1 0)] [] ))))
 
-(deftest move-forward-a-star-within-the-max-attempts-complete-the-level
+(deftest move-forward-a-star-within-the-max-movements-complete-the-level
     (is (level-completed? 1 [(at 1 0)] [:forward] )))
-  
 
+(deftest move-up-to-several-stars-within-the-max-movements-complete-the-level
+    (is (level-completed? 2 [(at 1 0) (at 2 0)] [:forward :forward])))
